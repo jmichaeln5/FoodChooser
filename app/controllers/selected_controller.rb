d9ce0abe23ab64044320e4ded3b7115ae3ceb4dd
Jon Norton <jmichaeln5@gmail.com>
2018-07-20 12:11:15 -0400

    fixed generated item page

--- app/controllers/selected_controller.rb
+++ app/controllers/selected_controller.rb
@@ -1,10 +1,14 @@
 class SelectedController < ApplicationController
 
+
+
   def random_item
+    @items = Item.all
     @selected_item = Item.all.shuffle.first
   end
 
   def random_restaurant
+    @restaurants = Restaurant.all
     @selected_restaurant = Restaurant.all.shuffle.first
   end
 
